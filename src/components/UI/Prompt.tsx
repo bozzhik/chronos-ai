e693f394b95009ec0a4b16caa615250c518db888
max <bozzhik@ya.ru>
2024-08-21 23:52:44 +0300

    finalize `Prompt` UI component

--- src/components/UI/Prompt.tsx
+++ src/components/UI/Prompt.tsx
@@ -3,17 +3,24 @@ import {cn} from '@/lib/utils'
 type Props = {
   type?: 'ai' | 'user'
   className?: string
-  text: string
+  text?: string
 }
 
 const promptVariants = {
+  default: 'font-advent !leading-none tracking-tight',
   ai: 'text-[90px] xl:text-6xl sm:text-4xl uppercase text-primary [text-shadow:_0_0_15px_var(--primary-shadow)]',
   user: 'text-[70px] xl:text-5xl sm:text-3xl lowercase text-secondary [text-shadow:_0_0_10px_var(--secondary-shadow)]',
 }
 
 export default function Prompt({type = 'ai', text, className}: Props) {
-  const Tag = type === 'ai' ? 'h1' : 'p'
-  const displayText = type === 'user' ? `<span class="block mt-2 sm:mt-1 tracking-[-0.15em]">${`>>`}</span> ${text}` : text
+  if (type === 'user') {
+    return (
+      <div className={cn('inline-flex gap-8 sm:gap-4', promptVariants.default, promptVariants[type], className)}>
+        <span className="block mt-2 sm:mt-1 tracking-[-0.15em]">{`>>`}</span>
+        <input type="text" className="bg-transparent border-none outline-none" />
+      </div>
+    )
+  }
 
-  return <Tag className={cn('font-advent !leading-none tracking-tight', type === 'user' ? 'inline-flex gap-8 sm:gap-4' : '', promptVariants[type], className)} dangerouslySetInnerHTML={{__html: displayText || ''}}></Tag>
+  return <h1 className={cn(promptVariants.default, promptVariants[type], className)} dangerouslySetInnerHTML={{__html: text || ''}}></h1>
 }
