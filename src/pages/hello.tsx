f736333e2862016ab9231e0b738351ba49c9c8b7
max <bozzhik@ya.ru>
2024-08-22 00:42:06 +0300

    init `Matrix` hello component (Pages)

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,10 +1,13 @@
 import Container from '#/Global/Container'
+import Matrix from '##/hello/Matrix'
 
 export default function HelloPage() {
   return (
     <Container className="flex flex-col gap-3 m-5">
       <h1 className="text-5xl uppercase font-advent">You are head north.</h1>
       <h2 className="font-sans text-2xl uppercase">Some text.</h2>
+
+      <Matrix />
     </Container>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '#': path.resolve(__dirname, './src/components'),
+      '##': path.resolve(__dirname, './src/components/Pages'),
     },
   },
   server: {
