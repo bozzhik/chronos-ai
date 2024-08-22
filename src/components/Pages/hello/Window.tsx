1dd577cd2056ce39b77151a4c3d85b8f4111210b
max <bozzhik@ya.ru>
2024-08-22 16:40:42 +0300

    finalize adaptive `Window` hello component using `@bozzhik/is-mobile`
    https://github.com/bozzhik/is-mobile

--- src/components/Pages/hello/Window.tsx
+++ src/components/Pages/hello/Window.tsx
@@ -1,7 +1,17 @@
+import {isMobile} from '@bozzhik/is-mobile'
+import {appPaths} from '@/lib/constants'
+
 import {Link} from 'react-router-dom'
 import Button from '#/UI/Button'
 
-import {appPaths} from '@/lib/constants'
+const windowData = {
+  heading: 'Ultra realistic <span class="duration-200 text-primary">AI</span> game',
+  text: {
+    desktop: 'Immerse yourself in an ultra-realistic AI-powered game where every decision shapes your escape from dynamic scenarios.',
+    mobile: 'Dive into an realistic AI game where decisions shape your escape from dynamic scenarios.',
+  },
+}
+const {heading, text} = windowData
 
 export default function Window() {
   return (
@@ -14,21 +24,19 @@ export default function Window() {
         </div>
       </div>
 
-      <div className="flex flex-col items-center justify-between h-full gap-6 px-24 mt-10 sm:px-2 xl:mt-7 py-28 sm:py-14">
-        <div className="flex flex-col items-center gap-3">
+      <div className="flex flex-col items-center justify-between h-full gap-6 px-24 mt-10 sm:gap-4 sm:px-2 xl:mt-7 py-28 sm:py-14">
+        <div className="flex flex-col items-center gap-3 sm:gap-4">
           <div className="inline-flex items-center justify-between border-[1.5px] min-w-0 rounded-full font-mono font-semibold px-3.5 py-1.5 text-xs gap-2 bg-neutral-900 border-neutral-800 uppercase">
             Powered by
             <a href="https://giga.chat/" target="_blank" className="duration-200 text-primary hover:underline hover:text-white">
               GigaChat
             </a>
           </div>
-          <h1 className="font-semibold tracking-tighter text-center text-7xl sm:text-5xl">
-            Ultra realistic <span className="duration-200 text-primary">AI</span> game
-          </h1>
-          <p className="text-xl sm:text-lg max-w-[50ch] sm:max-w-[25ch] font-medium tracking-tight text-center whitespace-pre-line text-neutral-400">Immerse yourself in an ultra-realistic AI-powered game where every decision shapes your escape from dynamic scenarios.</p>
+          <h1 className="font-semibold tracking-tighter text-center text-7xl sm:text-5xl" dangerouslySetInnerHTML={{__html: heading}} />
+          <p className="text-xl sm:text-lg max-w-[50ch] sm:max-w-[25ch] font-medium tracking-tight text-center whitespace-pre-line text-neutral-400"> {!isMobile ? text.desktop : text.mobile} </p>
         </div>
 
-        <div className="flex flex-col items-center gap-1.5">
+        <div className="flex flex-col items-center gap-1.5 sm:gap-1">
           <Button to={appPaths.playground} text="Try to escape" />
 
           <div className="group space-x-1.5 font-mono font-normal text-center whitespace-pre-line text-neutral-400">
