3e077d760ca64030c1323d06869e4a9d09d3d91c
max <bozzhik@ya.ru>
2024-08-22 13:26:05 +0300

    replace `Matrix` hello component with `react-matrix-animation` package
    https://github.com/philmetzger/react-matrix-animation

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,13 +1,17 @@
+import {ReactMatrixAnimation} from 'react-matrix-animation'
 import Container from '#/Global/Container'
-import Matrix from '##/hello/Matrix'
 
 export default function HelloPage() {
   return (
-    <Container className="flex flex-col gap-3 m-5">
-      <h1 className="text-5xl uppercase font-advent">You are head north.</h1>
-      <h2 className="font-sans text-2xl uppercase">Some text.</h2>
+    <Container className="grid h-screen place-items-center">
+      <div className="p-5 bg-black/15 backdrop-blur-md">
+        <h1 className="text-5xl uppercase font-advent">You are head north.</h1>
+        <h2 className="font-sans text-2xl uppercase">Some text.</h2>
+      </div>
 
-      <Matrix />
+      <div className="absolute inset-0 flex items-center justify-center w-screen h-screen m-auto -z-20">
+        <ReactMatrixAnimation tileSize={20} fadeFactor={0.07} fontColor="#233dcb" />
+      </div>
     </Container>
   )
 }
